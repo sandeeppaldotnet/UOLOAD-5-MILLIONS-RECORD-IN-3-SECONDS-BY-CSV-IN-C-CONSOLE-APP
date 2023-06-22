2aa13b4eee76f2cf5f378313ecfdade68e2ae5cb
sandeep084991904 <sandeep084991904@gmail.com>
2023-06-22 15:48:52 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upload_5_million_record_of_csv_file_by_c_sharp_dotnet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
